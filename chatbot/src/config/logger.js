8951c1dfd43d19ee06ef0df0256e9c3841738191
Manrajwazir <manrajwazir@gmail.com>
2025-07-01 16:34:54 -0600

    typo error in logger.js

--- chatbot/src/config/logger.js
+++ chatbot/src/config/logger.js
@@ -30,7 +30,7 @@ const logger = winston.createLogger({
 });
 
 if (process.env.NODE_ENV === 'development'){
-    logger.add(new winston.transports.console({
+    logger.add(new winston.transports.Console({
         format: winston.format.combine(
             winston.format.colorize(),
             winston.format.simple()
