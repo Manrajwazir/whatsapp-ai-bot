5da9bf63958920a18a9d80d0b4c1bd35215e2c5b
Manrajwazir <manrajwazir@gmail.com>
2025-07-30 16:19:50 -0600

    starting anew with chatController

--- chatbot/src/controllers/chatController.js
+++ chatbot/src/controllers/chatController.js
@@ -1,45 +1,4 @@
-const logger = require('../config/logger')
-const openaiService = require('../services/openaiService')
-
-class ChatController {
-  constructor () {
-    this.conversationHistory = []
-    this.maxHistoryLength = 30
-  }
-
-  async generateReply (userMessage) {
-    try {
-      this.addToHistory('user', userMessage)
-
-      const reply = await openaiService.generateReply(this.conversationHistory)
-
-      this.addToHistory('assistant', reply)
-      this.manageHistoryLength()
-
-      return reply
-    } catch (error) {
-      logger.error('Reply generation error:', error)
-      return null
-    }
-  }
-
-  addToHistory (role, content) {
-    this.conversationHistory.push({
-      role,
-      content,
-      timestamp: Date.now()
-    })
-  }
-
-  manageHistoryLength () {
-    if (this.conversationHistory.length > this.maxHistoryLength) {
-      this.conversationHistory = this.conversationHistory.slice(-this.maxHistoryLength)
-    }
-  }
-
-  getConversationHistory () {
-    return this.conversationHistory
-  }
-}
-
-module.exports = new ChatController()
+const logger = require("../config/logger");
+const openaiService = require("../services/openaiService");
+const profileService = require("../services/profileService");
+const chatHistoryService = require("../services/chatHistoryService");
